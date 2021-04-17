93427a93dd39e8ffe65868f74c967695c105e3c6
Basil Nikityuk <bazilio@equinox.local>
2021-04-17 17:42:19 +0300

    Initial Commit

---
+++ Sinewave/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sinewave
+//
+//  Created by Basil Nikityuk on 4/17/21.
+//  Copyright (c) 2021 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
